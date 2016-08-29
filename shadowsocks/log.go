305cbf786b6008ccb48cea8dec5659da1b02292c
Li Jinpei <leekingp1994@163.com>
2016-08-29 17:09:04 +0800

    1

--- shadowsocks/log.go
+++ shadowsocks/log.go
@@ -7,6 +7,7 @@ import (
 	"runtime"
 	"strconv"
 	"time"
+	"path"
 )
 
 const (
@@ -28,12 +29,14 @@ type logging struct {
 }
 
 // return time, file name, line number in string
-func (log *logging) Loghead(skip int) string {
+func (log *logging) Loghead(skip int) (tStr, pos string) {
 	t := time.Now()
-	tStr := t.Format(time.UnixDate)
+	tStr = t.Format(time.UnixDate)
 	_, file, line, _  := runtime.Caller(skip)
+	file = path.Base(file)
 	lineStr := strconv.Itoa(line)
-	return tStr + " " + file + " " + lineStr
+	pos = file + " " + lineStr
+	return
 }
 
 func (log *logging) MutexWrite(wr io.Writer, mes string, mut *sync.Mutex) {
@@ -48,18 +51,14 @@ func (log *logging) MutexWrite(wr io.Writer, mes string, mut *sync.Mutex) {
 func (log *logging) Write(level int, mes string, skip int) {
 	str, ok := log.LevelString[level]
 	if !ok {
-		// careful! don't incur never-end recursive call
-		io.WriteString(os.Stderr, "Log level not ok " + strconv.Itoa(level))
-		io.WriteString(os.Stderr, " map size " + strconv.Itoa(len(log.LevelString)))
-		io.WriteString(os.Stderr, "\n")
-		return
+		str = "unknown log level " + strconv.Itoa(level)
 	}
-	head := log.Loghead(skip + 1) + " " + str
-	mes1 := head + " " + mes
+	tStr, pos := log.Loghead(skip + 1)
+	mes = "[" + tStr + " " + str + " " + pos + "]" + mes
 	if level >= log.StderrLevel {
-		log.MutexWrite(os.Stderr, mes1, log.StderrMutex)
+		log.MutexWrite(os.Stderr, mes, log.StderrMutex)
 	}
-	log.MutexWrite(log.LogFile, mes1, log.FileMutex)
+	log.MutexWrite(log.LogFile, mes, log.FileMutex)
 }
 
 // (LogInit/LogFinish) and (LogChange/LogWrite) are not multithread-safa with each other
@@ -67,11 +66,11 @@ func (log *logging) Write(level int, mes string, skip int) {
 func (log *logging) LogInit(logFileName string, level int) {
 	log.LevelString = map[int]string{
 		CRITICAL : "CRITICAL",
-		ERROR    : "ERROR",
-		WARNING  : "WARNING",
-		INFO     : "INFO",
-		DEBUG    : "DEBUG",
-		NOTSET   : "NOTSET",
+		ERROR    : "ERROR   ",
+		WARNING  : "WARNING ",
+		INFO     : "INFO    ",
+		DEBUG    : "DEBUG   ",
+		NOTSET   : "NOTSET  ",
 	}
 	io.WriteString(os.Stderr, logFileName)
 	io.WriteString(os.Stderr, "\n")
--- shadowsocks/log_test.go
+++ shadowsocks/log_test.go
@@ -3,6 +3,10 @@ package shadowsocks
 import (
 	"testing"
 	"sync"
+	"math/rand"
+	"strconv"
+	"fmt"
+	"time"
 )
 
 func TestSerialLog(t *testing.T) {
@@ -10,7 +14,7 @@ func TestSerialLog(t *testing.T) {
 	log.LogInit("/tmp/test.log", DEBUG)
 	for level:= 0; level < CRITICAL; level += 10 {
 		for info:= 0; info < 100; info += 1 {
-			log.Write(level, "67890", 1)
+			log.Write(level, strconv.Itoa(info), 1)
 		}
 	}
 	log.LogFinish()
@@ -26,7 +30,7 @@ func TestParallelLog(t *testing.T) {
 			go func(level int, mes string, skip int) {
 				defer wg.Done()
 				log.Write(level, mes, skip)
-			}(level, "123456", 1)
+			}(level, strconv.Itoa(info), 1)
 		}
 	}
 	wg.Wait()
@@ -34,8 +38,52 @@ func TestParallelLog(t *testing.T) {
 }
 
 func TestSerialChange(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var log *logging = &logging{}
+	log.LogInit("/tmp/test.log", DEBUG)
+	for level:= 0; level < CRITICAL; level += 10 {
+		for info:= 0; info < 100; info += 1 {
+			log.Write(level, strconv.Itoa(info), 1)
+			if r := rand.Intn(2); 0 == r {
+				rFile := rand.Intn(100)
+				rLevel := rand.Intn(51)
+				rFileName := "/tmp/test.log." + strconv.Itoa(rFile)
+				rLevel = rLevel * 10
+				log.LogChange(rFileName, rLevel)
+				fmt.Print("log config changed " + rFileName + " " + strconv.Itoa(rLevel) + "\n")
+			}
+		}
+	}
+	log.LogFinish()
 }
 
 func TestParallelChange(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	var log *logging = &logging{}
+	var wg sync.WaitGroup
+	log.LogInit("/tmp/test.log", DEBUG)
+	for level:= 0; level < CRITICAL; level += 10 {
+		for info:= 0; info < 100; info += 1 {
+			wg.Add(1)
+			go func(level int, mes string, skip int) {
+				defer wg.Done()
+				log.Write(level, mes, skip)
+				if r := rand.Intn(2); 0 == r {
+					rFile := rand.Intn(100)
+					rLevel := rand.Intn(51)
+					rFileName := "/tmp/test.log." + strconv.Itoa(rFile)
+					rLevel = rLevel * 10
+					wg.Add(1)
+					go func(fFileName string, rLevel int) {
+						defer wg.Done()
+						log.LogChange(rFileName, rLevel)
+						fmt.Print("log config changed " + rFileName + " " + strconv.Itoa(rLevel) + "\n")
+					}(rFileName, rLevel)
+			}
+			}(level, strconv.Itoa(info), 1)
+		}
+	}
+	wg.Wait()
+	log.LogFinish()
 }
 
